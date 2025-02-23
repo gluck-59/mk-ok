8debb8896e1a46aaa2220baa461042bf8ee577fb
gluck <gluck59@gmail.com>
2025-02-23 17:08:12 +0500

    []

--- Okay/Core/UserReferer/UserReferer.php
+++ Okay/Core/UserReferer/UserReferer.php
@@ -35,7 +35,7 @@ class UserReferer
             Request::getReferer(),
             Request::getCurrentUrl()
         );
-
+prettyDump($referer);
         if ($referer->isKnown()) {
             switch ($referer->getMedium()) {
                 case self::CHANNEL_EMAIL :
